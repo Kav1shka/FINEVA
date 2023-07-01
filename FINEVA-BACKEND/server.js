c5e65db122803fe525bef2dac3a7c27dca7573c2
Kav1shka <103574964+Kav1shka@users.noreply.github.com>
2023-07-01 17:52:11 +0530

    create a server

---
+++ FINEVA-BACKEND/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+mongoose.set("strictQuery", false);
+
+const PORT = process.env.PORT || 8000;
+app.use(cors());
+
+app.use(bodyParser.json());
+
+const url ="mongodb+srv://Kav1shka:kavishka123@cluster0.6fyga0b.mongodb.net/";
+
+mongoose.connect(url, (err) => {
+  if (err) throw err;
+  console.log("MongoDB is connected...");
+});
+
+// app.use("/users", users)
+
+app.listen(PORT, () => {
+  console.log(`server running at http://localhost:${PORT}`);
+});
