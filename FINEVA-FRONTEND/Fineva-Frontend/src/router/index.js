9b057692efa7fc6bb3d4cd0430850faac507d632
Hiruna Ilukpitiya <33183435+HirunaIlukpitiya@users.noreply.github.com>
2023-07-03 21:07:54 +0530

    Hiruna's branch (#10)
    * test
    
    * Removed default files
    
    * test 2
    
    * add home page wih tailwind

--- FINEVA-FRONTEND/Fineva-Frontend/src/router/index.js
+++ FINEVA-FRONTEND/Fineva-Frontend/src/router/index.js
@@ -1,31 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
-<<<<<<< HEAD
-import HomeView from '../views/HomeView.vue'
-=======
->>>>>>> main
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-<<<<<<< HEAD
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path:"/",
+      name:"Home",
+      component:()=>import('../components/Home.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-=======
-
+      path:"/driverReg",
+      name:"driverReg",
+      component:()=>import('../components/driverReg.vue')
     },
     {
-
->>>>>>> main
+      path:"/policeReg",
+      name:"policeReg",
+      component:()=>import('../components/policeReg.vue')
+    },
+    {
+      path:"/signIn",
+      name:"signIn",
+      component:()=>import('../components/signin.vue')
     }
   ]
 })
