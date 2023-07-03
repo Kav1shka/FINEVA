4db395465ecd5d9289ea94c4a3d8618f8e2de625
HirunaIlukpitiya <33183435+HirunaIlukpitiya@users.noreply.github.com>
2023-07-03 18:00:45 +0530

    add home page wih tailwind

--- FINEVA-FRONTEND/Fineva-Frontend/src/router/index.js
+++ FINEVA-FRONTEND/Fineva-Frontend/src/router/index.js
@@ -4,10 +4,24 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-
+      path:"/",
+      name:"Home",
+      component:()=>import('../components/Home.vue')
     },
     {
-
+      path:"/driverReg",
+      name:"driverReg",
+      component:()=>import('../components/driverReg.vue')
+    },
+    {
+      path:"/policeReg",
+      name:"policeReg",
+      component:()=>import('../components/policeReg.vue')
+    },
+    {
+      path:"/signIn",
+      name:"signIn",
+      component:()=>import('../components/signin.vue')
     }
   ]
 })
