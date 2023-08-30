777789e64ff4ac7713d4c9b15f58adedefeb9328
hirunaIlukpitiya <hirunalakshan00@gmail.com>
2023-08-31 01:37:57 +0530

    password confirm added

--- FINEVA-BACKEND/Validations.js
+++ FINEVA-BACKEND/Validations.js
@@ -2,20 +2,21 @@ const registerValidDriver = (Email, Fname,Lname,Password,NIC,RegiNumber)=>{
   
     if (!Email) return "Please enter your email address";
     // if (!validateEmail(email)) return "Please enter valid email";
-    if (!Fname) return "Please enter your email address";
-    if (!Lname) return "Please enter your email address";
+    if (!Fname) return "Please enter your First Name ";
+    if (!Lname) return "Please enter your Last Name ";
     if (!Password) return "Please enter new password";
-    if (Password < 10)
+    if (Password.length < 10)
       return "Password should contain atleast 10 characters";
     if (!NIC) return "Please enter your NIC";
     if (!RegiNumber) return "Please enter your Registration Number";
 }
-const registerValidPoliceOfficer = (Email, Fname,Lname,Contact,RegiNumber)=>{
-      if (!Email) return "Please enter your Fname ";
-      if (!Fname) return "Please enter your Lname ";
-      if (!Lname) return "Please enter your Contact ";
-      if(!Contact) return "Please enter your NIC";
-      if (!RegiNumber) return "Please enter your RegiNumber ";
+const registerValidPoliceOfficer = (Email, Fname,Lname,Password,Contact,RegiNumber)=>{
+      if (!Email) return "Please enter your Email Address";
+      if (!Fname) return "Please enter your First Name";
+      if (!Lname) return "Please enter your Last Name ";
+      if(!Contact) return "Please enter your Contact Number";
+      if( Password.length < 10) return "Password should contain atleast 10 characters"
+      if (!RegiNumber) return "Please enter your Service ID ";
   }
  
     const DriverdloginValid = (NIC, Password) =>{
@@ -23,7 +24,7 @@ const registerValidPoliceOfficer = (Email, Fname,Lname,Contact,RegiNumber)=>{
       if (!Password) return "Please enter your pas7sword";
     };
     const OfficerloginValid = (RegiNumber, password) =>{
-      if (!RegiNumber) return "Please enter your RegiNumber";
+      if (!RegiNumber) return "Please enter your Service ID";
       if (!password) return "Please enter your password";
     };
    
